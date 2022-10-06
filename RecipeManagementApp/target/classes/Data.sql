f0b2457f71831194a3d0426c075a813489096523
sayalishrikhande <114572302+sayalishrikhande@users.noreply.github.com>
2022-10-06 18:41:23 +0530

    Delete RecipeManagementApp directory

--- RecipeManagementApp/target/classes/Data.sql
+++
@@ -1,5 +0,0 @@
-insert into recipe(id, name, instructions, ingredients, veg, servings, userID, created_Date, updated_Date) 
-values (100, 'Tea', 'Boil Tea', 'Tea, Milk, Sugar, Water' , '1' , '2' , '1', current_timestamp, current_timestamp);
-
-insert into recipe(id, name, instructions, ingredients, veg, servings, userID, created_Date, updated_Date) 
-values (101, 'Coffee', 'Boil all ingredients', 'Coffe, Milk, Sugar, Water' , '1' , '4' , '1', current_timestamp, current_timestamp);
