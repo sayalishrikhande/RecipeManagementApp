05d69e126371b0b3b8312e4c08d69052ac1340d1
sayalishrikhande <114572302+sayalishrikhande@users.noreply.github.com>
2022-10-06 13:21:54 +0530

    Delete RecipeManagementApp directory

--- RecipeManagementApp/src/main/java/com/sayali/springboot/repos/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sayali.springboot.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sayali.springboot.models.Recipe;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
-
-}
